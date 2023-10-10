1569e537ef4888383eaaf726cb2f9e462d93a5ac
TIAGO LIMA REIS <141974555+tiagobond@users.noreply.github.com>
2023-10-09 23:17:06 -0300

    Update sobre.js

---
+++ src/js/sobre.js
@@ -0,0 +1,43 @@
+//-----------Capturando elemento no DOM-----------//
+
+let pElements = document.querySelectorAll(".container p")
+pTexts = []
+pElements.forEach(function(pElement) {
+    pTexts.push(pElement.textContent)
+})
+
+//----------Criando novo elemento na DOM----------//
+
+let container = document.querySelector(".container");
+let novoParagrafo = document.createElement("p");
+novoParagrafo.textContent = "Obrigado pela visita em nosso site!!";
+
+container.appendChild(novoParagrafo);
+
+//----------Remover um elemento na DOM------------//
+
+let novoParagrafo = document.getElementById("novoParagrafo");
+elementoParaRemover.parentNode.removeChild(novoParagrafo);
+
+//-----------Criando o evento onclick-------------//
+
+function changeText() {
+    id.innerHTML = "Sobre";
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
